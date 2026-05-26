d6388bb0a0c199f55a3b48649d7e8584b9b46030
Pave <pitcheroty@gmail.com>
2026-05-26 20:53:36 +0200

    feature: create tools for use

---
+++ tools.py
@@ -0,0 +1,5 @@
+from langchain_community.tools import WikipediaQueryTool, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
+
