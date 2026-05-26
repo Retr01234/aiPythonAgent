5903ab68316fd00b1af5857b2aef5af5f87dbfbf
Pave <pitcheroty@gmail.com>
2026-05-26 20:06:48 +0200

    feature: Initialize Repo + 1st Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini")
